b0099a4ffe47df69daf12e312d95405881910f09
Pranav <baraiyaprnv@gmail.com>
2021-11-07 14:38:58 +0530

    Pizza add feature added

--- database/migrations/2021_11_07_055224_create_pizzas_table.php
+++ database/migrations/2021_11_07_055224_create_pizzas_table.php
@@ -16,6 +16,7 @@ class CreatePizzasTable extends Migration
         Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
             $table->integer('small_pizza_price');
             $table->integer('medium_pizza_price');
             $table->integer('large_pizza_price');
