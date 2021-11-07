d60bf414788c2d078c74110191e5c5ba9aba7fa4
Pranav <baraiyaprnv@gmail.com>
2021-11-07 15:17:37 +0530

    Pizza list added

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">All Pizza</div>
 
@@ -14,7 +14,37 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table table-dark table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Description</th>
+                            <th scoper="col">S.Price</th>
+                            <th scoper="col">M.Price</th>
+                            <th scoper="col">L.Price</th>
+                            <th scoper="col">Catogery</th>
+                            <th scoper="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($pizzas as $key=> $pizza)
+                            <tr>
+                            <th scope="row">{{$key+1}}</th>
+                            <td><img src="{{Storage::url($pizza->image)}}" width="80"></td>
+                            <td>{{$pizza->name}}</td>
+                            <td>{{$pizza->description}}</td>
+                            <td>{{$pizza->small_pizza_price}}</td>
+                            <td>{{$pizza->medium_pizza_price}}</td>
+                            <td>{{$pizza->large_pizza_price}}</td>
+                            <td>{{$pizza->catogery}}</td>
+                            <td><button class="btn btn-primary">Edit</button></td>
+                            <td><button class="btn btn-danger">Delete</button></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
